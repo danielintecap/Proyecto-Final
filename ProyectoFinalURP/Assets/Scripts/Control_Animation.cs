e8259e4c577d4673b950e3ef5f9df19de138a26c
danielgarcia3 <2023-000358@intecap.edu.gt>
2024-04-04 11:12:12 -0600

    personaje de chloe
    personaje con texturas, el modelo y las animaciones

--- ProyectoFinalURP/Assets/Scripts/Control_Animation.cs
+++ ProyectoFinalURP/Assets/Scripts/Control_Animation.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class Control_Animation : MonoBehaviour
 {
     // Start is called before the first frame update
-    Animator animator;
+    public Animator animator;
     const float AnimationTime = 0.05f;
     
     void Start()    
