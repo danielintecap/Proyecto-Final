390ef751d7dc33f937242ab270aa13b1e64b077b
danielgarcia3 <2023-000358@intecap.edu.gt>
2024-05-08 12:57:11 -0600

    Cambios armas
    Hice el cambio de las armas

--- ProyectoFinalURP/Assets/Scripts/Cambio_PJs.cs
+++ ProyectoFinalURP/Assets/Scripts/Cambio_PJs.cs
@@ -4,8 +4,8 @@ using UnityEngine;
 
 public class Cambio_PJs : MonoBehaviour
 {
-    public GameObject Chloe;
-    public GameObject DJ;
+    public GameObject Chloe, Arco, Ballesta;
+    public GameObject DJ, Bazuca, Hacha;
     public GameObject Luis;
     public GameObject IA_Chloe;
     public GameObject IA_DJ;
@@ -21,36 +21,67 @@ public class Cambio_PJs : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown("1"))
+        if (Input.GetKeyDown(KeyCode.J))
         {
             animator = GetComponentInChildren<Animator>();
             Chloe.SetActive(true);
             DJ.SetActive(false);
             Luis.SetActive(false);
+
             //IA
             IA_Chloe.SetActive(false);
             IA_DJ.SetActive(true);
             IA_Luis.SetActive(true);
+
+            //armas
+            if (Input.GetKeyDown("1"))
+            {
+                Arco.SetActive(true);
+                Ballesta.SetActive(false);
+                animator.SetBool("Boost", false);
+            }
+            if (Input.GetKeyDown("2"))
+            {
+                Ballesta.SetActive(true);
+                Arco.SetActive(false);
+                animator.SetBool("Boost", true);
+            }
         }
 
-        if (Input.GetKeyDown("2"))
+        if (Input.GetKeyDown(KeyCode.K))
         {
             animator = GetComponentInChildren<Animator>();
             Chloe.SetActive(false);
             DJ.SetActive(true);
             Luis.SetActive(false);
+
             //IA
             IA_Chloe.SetActive(true);
             IA_DJ.SetActive(false);
             IA_Luis.SetActive(true);
+
+            //armas
+            if (Input.GetKey("1"))
+            {
+                Bazuca.SetActive(true);
+                Hacha.SetActive(false);
+                animator.SetBool("Boost", false);
+            }
+            if (Input.GetKey("2"))
+            {
+                Hacha.SetActive(true);
+                Bazuca.SetActive(false);
+                animator.SetBool("Boost", true);
+            }
         }
 
-        if (Input.GetKeyDown("3"))
+        if (Input.GetKeyDown(KeyCode.L))
         {
             animator = GetComponentInChildren<Animator>();
             Chloe.SetActive(false);
             DJ.SetActive(false);
             Luis.SetActive(true);
+
             //IA
             IA_Chloe.SetActive(true);
             IA_DJ.SetActive(true);
