6d3cdde51126bba0338ce88919c1c0cb0a4a7c21
34JosueMarin <josmarhig04@gmail.com>
2024-06-24 10:24:35 -0600

    Cambie el nombre del script a MenuOptionSetting
    Cambie el nombre del script a MenuOptionSetting

--- ProyectoFinalURP/Assets/Script/MenuOptionSetting.cs
+++ ProyectoFinalURP/Assets/Script/MenuOptionSetting.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Audio;
 
-public class MenuOptions : MonoBehaviour
+public class MenuOptionSetting : MonoBehaviour
 {
     public AudioMixer audioMixer;
 
